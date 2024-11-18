da54c22047ec043808b21209ec6f9615e8b6342f
roman <rrr@dsds.ru>
2024-11-18 15:53:58 +0300

    создали на 4 уроке

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,11 +1,6 @@
 <?php
-
 define('DEBUG_FILE_NAME', $_SERVER["DOCUMENT_ROOT"] .'/logs/'.date("Y-m-d").'.log');
 
 if (file_exists(__DIR__ . '/classes/autoload.php')) {
     require_once __DIR__ . '/classes/autoload.php';
-}
-
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require_once __DIR__ . '/vendor/autoload.php';
 }
