49496dadaf6eb90e444f8586c51d51d66599b985
roman <rrr@dsds.ru>
2024-11-27 20:07:52 +0300

    пушим по ДЗ по уроку 6

--- doctors/doctor.php
+++ doctors/doctor.php
@@ -15,18 +15,39 @@ $iblockId = 19;
 $iblockElementId = $_GET['id_doctor'];
 
 $iblock = Iblock::wakeUp($iblockId);
-//$element = $iblock->getEntityDataClass()::getByPrimary($iblockElementId)->fetchObject();
 
 $element = $iblock->getEntityDataClass()::getByPrimary(
     $iblockElementId,
-    ['select' => ['NAME', 'LAST', 'FIRST', 'SURNAME', 'PROCEDURES']])
+    ['select' => ['NAME', 'LAST', 'FIRST', 'SURNAME']])
     ->fetchObject();
 
 $fio = $element->get('LAST')->getValue().' '.$element->get('FIRST')->getValue().' '.$element->get('SURNAME')->getValue();
-$procedures = $element->get('PROCEDURES');
+
+$element = \Bitrix\Iblock\Elements\ElementDoctorsTable::getList([
+    'select' => ['ID', 'NAME', 'LAST', 'FIRST', 'SURNAME', 'PROCEDURES'],
+    'filter' => [
+        'ID' => $iblockElementId,
+    ],
+])->fetchObject();
+
+$procedures = "";
+
+foreach ($element->getProcedures()->getAll() as $section) {
+    //$procedures .= $section->getValue() . '<br>';
+
+    $element = \Bitrix\Iblock\Elements\ElementProceduresTable::getList([
+        'select' => ['ID', 'NAME'],
+        'filter' => [
+            'ID' => $section->getValue(),
+        ],
+    ])->fetchObject();
+
+    $procedures .= $element->getName() . '<br>';
+}
 
 echo "<h2 style='color: #666666'>".$fio."</h2>";
-echo "<div>Процедуры:</div>";
-//echo "<div>".$procedures."</div>";
+echo "<div><b>Процедуры:</b></div>";
+echo "<div>".$procedures."</div><br/>";
+echo "<div><a href='/doctors/'>Вернуться на главную</a></div>";
 
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
